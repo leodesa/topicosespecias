63c4fac8cc71a526fd54cdb73d57ab9676af06dc
Leonardo Sá <leonardo231198@gmail.com>
2018-08-21 19:51:59 -0300

    Primeiro trabalho

---
+++ AulaGit/src/aulagit/AulaGit.java
@@ -0,0 +1,16 @@
+package aulagit;
+
+/**
+ *
+ * @author leona
+ */
+public class AulaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Pau no cu de quem ta lendo!");
+    }
+    
+}
