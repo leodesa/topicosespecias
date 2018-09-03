be230dec7086500514d11de64a2a5f60d7e01b6c
Leonardo Sá <leonardo231198@gmail.com>
2018-09-03 20:04:35 -0300

    Aula JPA

--- src/fvs/edu/dominio/Aluno.java
+++ src/fvs/edu/dominio/Aluno.java
@@ -2,14 +2,31 @@ package fvs.edu.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class Aluno implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	private String cpf;
 	private String rg;
 	private Integer matricula;
+	
+	public Aluno(Integer id, String nome, String cpf, String rg, Integer matricula) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.rg = rg;
+		this.matricula = matricula;
+	}
 	
 	@Override
 	public String toString() {
@@ -20,60 +37,45 @@ public class Aluno implements Serializable{
 		return id;
 	}
 
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-
 
 	public String getCpf() {
 		return cpf;
 	}
 
-
-
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-
-
 
 	public String getRg() {
 		return rg;
 	}
 
-
-
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
-
-
 
 	public Integer getMatricula() {
 		return matricula;
 	}
 
-
-
 	public void setMatricula(Integer matricula) {
 		this.matricula = matricula;
 	}
 
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 	@Override
 	public int hashCode() {
@@ -86,6 +88,7 @@ public class Aluno implements Serializable{
 		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -122,7 +125,5 @@ public class Aluno implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 	
 }
