c9c3334951d17748dde03da851ef2583b09684e5
Leonardo <leonardo231198@gmail.com>
2018-10-01 20:00:05 -0300

    Implementação do Dominio Categoria

--- ProjetoTopicosEspecias/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciasApplication.java
+++ ProjetoTopicosEspecias/src/main/java/fvs/edu/br/topicos/ProjetoTopicosEspeciasApplication.java
@@ -1,12 +1,33 @@
 package fvs.edu.br.topicos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoTopicosEspeciasApplication {
+import fvs.edu.br.topicos.domain.Categoria;
+import fvs.edu.br.topicos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoTopicosEspeciasApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoriaRepository catRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTopicosEspeciasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+	
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 }
